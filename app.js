dcf3d2f4e7ef128446202727078f977190e3d703
Yoda-Flash <kaitlyn.zee@gmail.com>
2025-06-09 15:39:52 +0800

    Added switch theme

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-import {Image_Generator} from "./image_generator.js";
+// import {Image_Generator} from "./image_generator.js";
 
-let imageGenerator = new Image_Generator();
+// let imageGenerator = new Image_Generator();
+let switchTheme = document.getElementById('switch-theme');
+let toggleTheme = document.getElementById('toggle-theme');
+let themes = ["light", "dark", "cupcake", "synthwave", "valentine", "aqua", "fantasy", "night", "winter", "caramellatte", "abyss"];
+let themesCounter = 0;
+
 let accountID = document.getElementById('account-id');
 let apiKey = document.getElementById('api-key');
 let occasion = document.getElementById('occasion');
@@ -12,8 +17,16 @@ let image = document.getElementById('image');
 let downloadButton = document.getElementById('download');
 let formattedLikes = '';
 
-generateButton.onclick = () => {
-    imageGenerator.generate(createPromptFromInputs(), getAccountID(), getAPIKEY())
+// generateButton.onclick = () => {
+//     imageGenerator.generate(createPromptFromInputs(), getAccountID(), getAPIKEY())
+// }
+
+switchTheme.onclick = () => {
+    document.querySelector('html').setAttribute('data-theme', themes[themesCounter]);
+    themesCounter += 1;
+    if (themesCounter === themes.length){
+        themesCounter = 0;
+    }
 }
 
 let createPromptFromInputs = () => {
