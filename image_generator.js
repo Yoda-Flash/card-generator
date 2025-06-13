7789765d5d8e6ca573325462bec59e2b3dad54a6
Yoda-Flash <kaitlyn.zee@gmail.com>
2025-06-13 11:27:46 +0800

    Added the HTML/CSS/JS static files to the server so I don't need to host it on GitHub Pages in addition

---
+++ image_generator.js
@@ -0,0 +1,73 @@
+// import dotenv from 'dotenv';
+// import fs from 'fs';
+
+
+export class Image_Generator {
+    constructor() {
+        // dotenv.config();
+    }
+
+    generate = async (prompt, username, api_key) => {
+
+        let image = ''
+
+        let response = await fetch(
+            `https://card-generator.yoda-flash.hackclub.app/image/${prompt}`, {
+                method: "GET"
+            }
+        ).then(response => response.text().then(result => image = result))
+        .catch(err => console.log(err));
+
+        return image;
+
+        // let url = `https://api.cloudflare.com/client/v4/accounts/${username}/ai/run/@cf/black-forest-labs/flux-1-schnell`;
+        //
+        // let response = await fetch(
+        //     url, {
+        //         mode: "cors",
+        //         credentials: "include",
+        //         method: 'POST',
+        //         headers: {
+        //             "Access-Control-Allow-Origin": new URL(url).origin,
+        //             "Access-Control-Expose-Headers": "*",
+        //             "Access-Control-Request-Method": "POST",
+        //             "Access-Control-Request-Headers": "*",
+        //             "Access-Control-Allow-Headers": "*",
+        //             "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
+        //             "Access-Control-Max-Age": 86400,
+        //             "Access-Control-Allow-Credentials": true,
+        //             "Origin": new URL(url).origin,
+        //             "Authorization": `Bearer ${api_key}`,
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify({
+        //             "prompt": prompt,
+        //             "seed": Math.floor(Math.random() * 10)
+        //         })
+        //     }
+        // ).catch(err => console.log(err));
+        //
+        // let text = await response.text();
+        // let image = JSON.parse(text).result.image;
+        // // fs.writeFileSync("image.txt", "data:image/jpeg;base64," + image);
+        // return "data:image/jpeg;base64," + image;
+        // let buffer = Buffer.from(image, 'base64');
+        //
+        // fs.writeFileSync("image.jpeg", buffer);
+        //
+        // return new Response(buffer, {
+        //     headers: {
+        //         'Content-Type': 'image/jpeg',
+        //         'Access-Control-Allow-Origin': '*',
+        //         'Access-Control-Allow-Headers': "*"
+        //     },
+        // });
+    }
+}
+
+// async function main() {
+//     const imageGenerator = new Image_Generator();
+//     await imageGenerator.generate("Please generate a green Father's Day day card for Patrick, who likes money and gold.", getAccount(), getApiKey()).catch(err => console.log(err));
+// }
+
+// main().catch(err => console.log(err));
---
+++ script.js
@@ -0,0 +1,86 @@
+import {Image_Generator} from "./image_generator.js";
+
+let imageGenerator = new Image_Generator();
+let switchTheme = document.getElementById('switch-theme');
+let themes = ["light", "cupcake", "synthwave", "valentine", "aqua", "fantasy", "night", "winter", "caramellatte", "abyss"];
+let themesCounter = 0;
+
+let accountID = document.getElementById('account-id').innerText;
+let apiKey = document.getElementById('api-key').innerText;
+let generateButton = document.getElementById('generate');
+let image = document.getElementById('image');
+let downloadButton = document.getElementById('download');
+
+let likesArray = [];
+let formattedLikes = '';
+
+let prompt = '';
+
+generateButton.onclick = async () => {
+    let imageURL = await imageGenerator.generate(createPromptFromInputs(), getAccountID(), getAPIKEY()).catch(err => console.log(err));
+    const response = await fetch(imageURL);
+    const imageBlob = await response.blob();
+    image.src = URL.createObjectURL(imageBlob);
+}
+
+switchTheme.onclick = () => {
+    document.querySelector('html').setAttribute('data-theme', themes[themesCounter]);
+    themesCounter += 1;
+    if (themesCounter === themes.length){
+        themesCounter = 0;
+    }
+}
+
+downloadButton.onclick = () => {
+    let link = document.createElement('a');
+    link.setAttribute('href', image.src);
+    link.setAttribute('download', "image.jpeg");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+let createPromptFromInputs = () => {
+    let occasion = document.getElementById('occasion').value.toLowerCase();
+    let recipientName = document.getElementById('name').value.toLowerCase();
+    let color = document.getElementById('color').value.toLowerCase();
+    let likes = document.getElementById('likes').value.toLowerCase().replaceAll(" ", "");
+    if (likes !== '') {
+        prompt = `Please generate a ${color} ${occasion} card for ${recipientName}, who likes ${formatLikes(likes)}.`;
+    } else {
+        prompt = `Please generate a ${color} ${occasion} card for ${recipientName}.`;
+    }
+    console.log(prompt);
+    return prompt;
+}
+
+let formatLikes = (likes) => {
+    likesArray = [];
+    formattedLikes = '';
+    console.log(likes);
+    likesArray = likes.replaceAll(" ", "").split(",");
+    for (let i=0; i<likesArray.length; i++) {
+        if ((likesArray.length > 1)) {
+            if ((i === likesArray.length - 1)) {
+                formattedLikes += `and ${likesArray[i]}`;
+            } else {
+                formattedLikes += `${likesArray[i]}, `
+            }
+        } else if ((likesArray.length === 1)) {
+            formattedLikes += likesArray[i];
+        }
+    }
+    return formattedLikes;
+}
+
+let getAccountID = () => {
+    if (accountID.value !== '') {
+        return accountID.value;
+    } else return "none";
+}
+
+let getAPIKEY = () => {
+    if (apiKey.value !== '') {
+        return apiKey.value;
+    } else return "none";
+}
